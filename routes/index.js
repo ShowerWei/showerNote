35a9b29d30a287cccd17896cc57b9a4b179ceeec
Showerwei <ie965217@gmail.com>
2014-09-25 16:29:15 +0000

    API: http get post

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'showerNote' });
 });
+
+// more routes for our API will happen here
+router.route('/items')
+  .get(function(req, res){
+    fs.readFile('./todo.json', 'utf8', function (err, data) {
+      if (err) { throw err; }
+      res.set({"Access-Control-Allow-Origin": "*"});
+      res.setHeader('Content-Type', 'application/json');
+      res.end(data);
+    });
+  })
+   //create a todo item
+  .post(function(req, res){
+    var parseTodo;
+    var todo ={
+      stat : req.body.stat,
+      text : req.body.text
+    };
+
+    fs.readFile('./todo.json', function (err, data) {
+      if (err) throw err;
+      parseTodo = JSON.parse(data);
+      parseTodo.todoList.push(todo);
+      console.log(parseTodo);
+    });     
+
+    var json = JSON.stringify(parseTodo);
+    fs.writeFile('./todo.json',  json, function (err) {
+      if (err) throw err;
+      console.log('It\'s saved!');
+    });
+//todo.save(function(err){
+    //  if(err)
+    //    res.send(err);
+    res.json({"message": "todo created"});
+    //});
+  });
+
+
 
 module.exports = router;
