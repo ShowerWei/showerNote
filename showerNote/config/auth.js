419d2dbbb4c2190a1d543d668d109b5eeeb29e70
ryan.yw_lin <ryan.yw_lin@htc.com>
2016-09-07 14:14:24 +0800

    Remove node_module

--- showerNote/config/auth.js
+++ showerNote/config/auth.js
@@ -4,9 +4,9 @@
 module.exports = {
 
 	'facebookAuth' : {
-		'clientID' 		: '1469441416678064', // your App ID
-		'clientSecret' 	: '50a723f90cb7443e20fbb79b66b8d4f1', // your App Secret
-		'callbackURL' 	: 'http://54.64.202.70:3000/auth/facebook/callback'
+		'clientID' 	: 'your-clientID-here', // your App ID
+		'clientSecret' 	: 'your-client-secret-here', // your App Secret
+		'callbackURL' 	: 'http://localhost:3000/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
